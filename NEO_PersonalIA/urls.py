7b5c8b99231b21f4f268350f93c90caff5853734
Bruno Wandermurem <brunowandermurem.juris@gmail.com>
2025-07-31 23:25:56 -0300

    Update urls.py to include product and order management views

--- NEO_PersonalIA/urls.py
+++ NEO_PersonalIA/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from .views import add_product, product_list, create_order
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add-product/', add_product, name='add_product'),
+    path('products/', product_list, name='product_list'),
+    path('create-order/', create_order, name='create_order'),
 ]
