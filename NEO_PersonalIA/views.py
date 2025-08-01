409af26b6b2add9822bc32cae1cf9944259da2cb
Bruno Wandermurem <brunowandermurem.juris@gmail.com>
2025-07-31 23:24:46 -0300

    Add views for Product and Order management

--- NEO_PersonalIA/views.py
+++ NEO_PersonalIA/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
-from .models import UserInteraction
+from django.shortcuts import render, redirect
+from .models import User, Product, Order
+from .forms import ProductForm, OrderForm
 
-# View for displaying user interactions
+# View for displaying and adding products
 
-def index(request):
-    interactions = UserInteraction.objects.all()
-    return render(request, 'index.html', {'interactions': interactions})
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'add_product.html', {'form': form})
+
+# View for displaying products
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+# View for creating an order
+
+def create_order(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('order_summary')
+    else:
+        form = OrderForm()
+    return render(request, 'create_order.html', {'form': form})
